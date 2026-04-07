6d2138015fe2fa8fe912add2dbfd10930d1ebc5a
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2026-04-07 15:02:58 +0800

    feat: add catppuccin

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -33,6 +33,7 @@ let
   all-plugins = with pkgs.vimPlugins; [
     # plugins from nixpkgs go in here.
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
+    catppuccin-nvim
   ];
 
   extraPackages = with pkgs; [
---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,6 @@
+require('catppuccin').setup {
+  flavour = 'mocha', -- latte, frappe, macchiato, mocha
+}
+
+-- Load the colorscheme
+vim.cmd.colorscheme('catppuccin')
